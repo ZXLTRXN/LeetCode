94d20ea7f92d0f455901e1593bc65a574c3d81dc
Ilya Shevtsov <shevtsovis@novotelecom.ru>
2025-09-17 18:15:49 +0800

    append examples

--- src/main/kotlin/live_coding/Task_8_flow_in_vm.kt
+++ src/main/kotlin/live_coding/Task_8_flow_in_vm.kt
@@ -1,6 +1,33 @@
 package live_coding
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.transformLatest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Сделайте рефакторинг кода,
@@ -9,19 +36,88 @@ import kotlinx.coroutines.flow.MutableStateFlow
  * и отдает для экрана observable screen state.
  */
 
-class Model()
+data class Model(val id: Long)
 
 interface Interactor {
-
     suspend fun getModels(query: String): List<Model>
 }
 
-class ScreenViewModel(private val interactor: Interactor) {
 
-    val list: MutableStateFlow<List<Model>> = MutableStateFlow(emptyList())
+class ScreenViewModel(
+    private val interactor: Interactor,
+    private val defaultDispatcherProvider: suspend () -> CoroutineContext = { Dispatchers.Default }
+) {
+
+    private val viewModelScope = CoroutineScope(SupervisorJob())
+
+    private val _userInput = MutableStateFlow<String>("")
+
+    private val debounceMs = 500L
+
+    private val state: StateFlow<ScreenState> = _userInput // 1 источник стейта
+//        .distinctUntilChanged() оно уже встроено в StateFlow так то
+        .validate() // по приколу написал
+        .transformLatest { (query, isValid) ->
+            emit(ScreenState.Typing(query)) // дает мгновенный отклик на ввод, чтобы например подсказки показать
+            if (query.isBlank()) {
+                emit(ScreenState.Idle)
+                return@transformLatest
+            }
+            delay(debounceMs)
+
+            emit(ScreenState.Loading(query))
+            val resultState = getModelsState(query)
+            emit(resultState)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ScreenState.Idle
+        )
+
+
+    // просто интересно как реализовать
+    private fun Flow<String>.validate(): Flow<Pair<String, Boolean>> {
+        val pattern = Regex("[A-Za-z0-9]+")
+
+        return flow {
+            collect {
+                 val isValid = withContext(defaultDispatcherProvider()) {
+                    pattern.matches(it)
+                }
+                emit(it to isValid)
+            }
+        }
+    }
 
     fun onTextChanged(value: String) {
-        //todo realization
+        _userInput.value = value
+    }
+
+    private suspend fun getModelsState(query: String): ScreenState {
+        return try {
+            val models = interactor.getModels(query)
+            if (models.isEmpty()) {
+                ScreenState.Empty(query)
+            } else {
+                ScreenState.Loaded(query, models)
+            }
+        } catch (ex: Exception) {
+            ScreenState.Error(query,"error")
+        }
+    }
+
+    fun onCleared() {
+        viewModelScope.cancel()
+    }
+
+
+    sealed interface ScreenState {
+        data object Idle: ScreenState // инициализационный стейт - хорошая практика
+        data class Typing(val query: String): ScreenState
+        data class Loading(val query: String): ScreenState
+        data class Empty(val query: String): ScreenState
+        data class Loaded(val query: String, val models: List<Model>): ScreenState
+        data class Error(val query: String, val message: String): ScreenState
     }
 
 }
